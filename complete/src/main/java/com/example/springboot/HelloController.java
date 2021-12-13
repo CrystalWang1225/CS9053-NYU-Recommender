0195a85668f4691e97b32b049f5907175d79cf13
Simay <simay1224@gmail.com>
2021-12-13 02:01:54 -0500

    implemented basic REST APIs on the HelloController file

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,59 +1,38 @@
 package com.example.springboot;
 
+import com.example.springboot.services.MovieServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//jsoup
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
-	// imdb URL links for each genre
-	final String imdb_comedy_link = "https://www.imdb.com/search/title/?genres=comedy&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3396781f-d87f-4fac-8694-c56ce6f490fe&pf_rd_r=3SXPNY4EJJ8DB24QT8S3&pf_rd_s=center-1&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr1_i_1";
-	final String imdb_romance_link = "https://www.imdb.com/search/title/?genres=romance&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e0da8c98-35e8-4ebd-8e86-e7d39c92730c&pf_rd_r=3SXPNY4EJJ8DB24QT8S3&pf_rd_s=center-2&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr2_i_1";
-	final String imdb_scifi_link = "https://www.imdb.com/search/title/?genres=sci-fi&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3396781f-d87f-4fac-8694-c56ce6f490fe&pf_rd_r=3SXPNY4EJJ8DB24QT8S3&pf_rd_s=center-1&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr1_i_2";
-	final String imdb_action_link = "https://www.imdb.com/search/title/?genres=action&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e0da8c98-35e8-4ebd-8e86-e7d39c92730c&pf_rd_r=3SXPNY4EJJ8DB24QT8S3&pf_rd_s=center-2&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr2_i_2";
-	final String imdb_drama_link = "https://www.imdb.com/search/title/?genres=drama&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=f1cf7b98-03fb-4a83-95f3-d833fdba0471&pf_rd_r=3SXPNY4EJJ8DB24QT8S3&pf_rd_s=center-3&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr3_i_1";
 
-	//global variables
-	String title, genre, rating;
-
+	@Autowired
+	private MovieServiceImplementation movieService;
 
 	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public List<Movie> index() {
+		System.out.println("Greetings from Spring Boot!");
+		return (List<Movie>) movieService.findAll();
+
 	}
 
-	@GetMapping("/scrape")
-	public String scrape() throws  Exception{
-		//scraping each genre seperately and printing the results  out
-		System.out.println("starting romance movies");
-		scrape_url(imdb_comedy_link);
-		System.out.println("-----------------------------------------------------------------------");
-		scrape_url(imdb_romance_link);
-		System.out.println("-----------------------------------------------------------------------");
-		scrape_url(imdb_scifi_link);
-		System.out.println("-----------------------------------------------------------------------");
-		scrape_url(imdb_action_link);
-		System.out.println("-----------------------------------------------------------------------");
-		scrape_url(imdb_drama_link);
-		System.out.println("-----------------------------------------------------------------------");
-
-		//return string
-		return "finished scraping";
+	@GetMapping("/search/{name}")
+	List<Movie>  getByName(@PathVariable String name) {
+		//example request: http://localhost:8080/search/Encanto
+		return movieService.findByName(name);
+			//	.orElseThrow(() -> new EmployeeNotFoundException(id));
 	}
-	public void scrape_url(String url_link)throws  Exception{
-		// given a imdb URL address that contains movies for specific genre,
-		// this method scrapes the movies and prints out the result for each movie
-		final Document document_romance = Jsoup.connect(url_link).get();
-		for (Element row:document_romance.getElementsByClass("lister-item-content")){
-			title= row.select(".lister-item-header").select("a[href]").text();
-			genre= row.select(".genre").text();
-			rating= row.select(".ratings-imdb-rating").attr("data-value");
-			System.out.println(title + " genre: " + genre + " rating: " + rating  );
 
-		}
+	@GetMapping("/search")
+	List<Movie>  getByRating(@RequestParam String min, @RequestParam String max) {
+		// example request : http://localhost:8080/search?min=7.0&max=9.0
+		return movieService.searchByRating(Double.parseDouble(min),Double.parseDouble(max));
 	}
+
 }
