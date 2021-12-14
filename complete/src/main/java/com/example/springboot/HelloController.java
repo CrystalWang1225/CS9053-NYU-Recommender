196fd0ac76dd86be3cdca5fabf05b7784af31619
Crystal Wang <crystalwang@Crystals-iMac.lan1>
2021-12-14 11:13:00 -0500

    Added cors for backend and added slider connection

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
@@ -23,6 +23,7 @@ public class HelloController {
 	}
 
 	@GetMapping("/search/{name}")
+	@CrossOrigin(origins = "http://localhost:3000")
 	List<Movie>  getByName(@PathVariable String name) {
 		//example request: http://localhost:8080/search/Encanto
 		return movieService.findByName(name);
@@ -30,6 +31,7 @@ public class HelloController {
 	}
 
 	@GetMapping("/search")
+	@CrossOrigin(origins = "http://localhost:3000")
 	List<Movie>  getByRating(@RequestParam String min, @RequestParam String max) {
 		// example request : http://localhost:8080/search?min=7.0&max=9.0
 		return movieService.searchByRating(Double.parseDouble(min),Double.parseDouble(max));
